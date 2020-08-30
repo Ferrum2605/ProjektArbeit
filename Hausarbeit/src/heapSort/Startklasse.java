14d3209179f2d2e765542fdf57dde09c978d2020
A002520 <A002520@NB4535.all-for-one.net>
2020-08-30 13:10:13 +0200

    Surface

--- Hausarbeit/src/heapSort/Startklasse.java
+++ Hausarbeit/src/heapSort/Startklasse.java
@@ -5,50 +5,45 @@ public class Startklasse {
     public static void main(String[] args) {
 
         int [] array = {55, 22, 15, 12, 48, 36, 88, 85};
-        //hallo
         
-
         heapSort(array);
     }
     
-    
-    //n = letzte Stelle des Baumes die betrachet werden soll
-    
     public static void heapSort(int[] array) {
-        generateMaxHeap(array);							// Erstellung BinärBaum
-        printArray(array);								
-        for(int index = array.length - 1; index > 0; index--) {
-            swap(array, index, 0);									
+    	
+        generateMaxHeap(array);	
+        printArray(array);
+        
+        for(int lastIndex = array.length - 1; lastIndex > 0; lastIndex--) {
+            swap(array, lastIndex, 0);									
             printArray(array);
-            compareAndSwap(array, 0, index);
+            compareAndSwap(array, 0, lastIndex);
             printArray(array);
         }
     }
     
-    //Erstellt MaxHeapBaum im Array
     public static void generateMaxHeap(int[] array) {
 
-        for(int i = (array.length / 2) - 1; i >= 0; i--) {		// geteilt durch 2, da bei jedem Swap 2 Zahlen vertauscht werden und Index bei 0 beginnt
+        for(int i = (array.length / 2) - 1; i >= 0; i--) {		
             compareAndSwap(array, i, array.length);
-            //System.out.println(i);
+            
         }
     }
-     
+    
     public static void compareAndSwap(int[] array, int parentIndex, int lastIndex) {
+    														
+        while(parentIndex <= (lastIndex / 2) - 1) {			
+            int childIndex = ((parentIndex + 1) * 2) - 1; 	
 
-        while(parentIndex <= (lastIndex / 2) - 1) {
-            int childIndex = ((parentIndex + 1) * 2) - 1;  //Index des linken Kindes
-
-            if (childIndex + 1 <= (lastIndex - 1)) {	//Prüfung, ob es ein Parent hat
-                if(array[childIndex] < array[childIndex + 1]) {  //Vergleich der Kinder 
+            if (childIndex + 1 <= (lastIndex - 1)) {			
+                if(array[childIndex] < array[childIndex + 1]) {   
                     childIndex++;
                 }
             }
 
-            //Prüfung ob Swap nötig ist
             if(array[parentIndex] < array[childIndex]) {
                 swap(array, parentIndex, childIndex);
-                parentIndex = childIndex;				
+                parentIndex = childIndex;						
             }else break;
         }
 
@@ -60,12 +55,9 @@ public class Startklasse {
         array[parentIndex] = array[childIndex];
         array[childIndex] = z;
     }
-
-   
-   
-
     
-     public static void printArray(int arr[]) 			//Ausgabe, nur schön mit zweistelligen Werten ;)
+    
+     public static void printArray(int arr[]) 			
         { 
             int n = arr.length; 
            
