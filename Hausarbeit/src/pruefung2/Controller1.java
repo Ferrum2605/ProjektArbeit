55716ecc49a659966fd04f5374186bcbe45ef195
rapha <rapha@DESKTOP-5JOV5DA>
2020-09-07 09:44:12 +0200

    ...

---
+++ Hausarbeit/src/pruefung2/Controller1.java
@@ -0,0 +1,40 @@
+package pruefung2;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.event.ActionEvent;
+
+public class Controller1 {
+
+	@FXML
+	MenuBar menuBar;
+	@FXML
+	Menu fileMenu;
+	@FXML
+	MenuItem newFileMenuItem;
+	@FXML
+	MenuItem openFileMenuItem;
+	@FXML
+	MenuItem saveFileMenuItem;
+	@FXML
+	MenuItem saveFileAsMenuItem;
+
+	@FXML
+	public void newFile(ActionEvent event) {
+	}
+
+	@FXML
+	public void openFile(ActionEvent event) {
+	}
+
+	@FXML
+	public void saveFile(ActionEvent event) {
+	}
+
+	@FXML
+	public void saveFileAs(ActionEvent event) {
+	}
+
+}
