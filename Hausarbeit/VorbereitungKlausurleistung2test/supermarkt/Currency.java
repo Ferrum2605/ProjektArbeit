501a4fc9e7c3d6d96d35d3836575bb6f6c1e565a
Ferrum2605 <60215296+Ferrum2605@users.noreply.github.com>
2020-09-08 16:37:56 +0200

    Jannik

---
+++ Hausarbeit/VorbereitungKlausurleistung2test/supermarkt/Currency.java
@@ -0,0 +1,31 @@
+package supermarkt;
+
+public enum Currency {
+	EURO("Euro", '€'), DOLLAR("Dollar", '$');
+	
+	private String description;
+	private char currencyCode;
+	
+	Currency(String bezeichnung, char zeichen)
+	{
+		this.description = bezeichnung;
+		this.currencyCode = zeichen;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public char getCurrencyCode() {
+		return currencyCode;
+	}
+
+	public void setCurrencyCode(char currencyCode) {
+		this.currencyCode = currencyCode;
+	}
+
+}
---
+++ Hausarbeit/VorbereitungKlausurleistung2test/supermarkt/SuperMarket.java
@@ -0,0 +1,95 @@
+package supermarkt;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class SuperMarket {
+	
+	private String name;
+	private ArrayList<Goods> goods;
+	
+	public SuperMarket(String name)
+	{
+		this.name = name;
+		goods = new ArrayList<>();
+	}
+	
+	public void uploadGoods() 
+	{
+		File file =new File("C:\\Users\\Jannik\\git\\ProjektArbeit\\VorbereitungKlausurleistung2\\src\\supermarkt\\waren.txt");
+		
+		
+		try (FileReader fr = new FileReader(file); BufferedReader br = new BufferedReader(fr)) {
+			Scanner scanner = new Scanner(file);
+			
+			while (scanner.hasNextLine()) {
+				String line = scanner.nextLine();
+				String[] tokens = line.split(";");
+				int id = Integer.parseInt(tokens[0]);
+				String des = tokens[1];
+				String st1 = tokens[2];
+				ClassOfGoods classOfGoods = null;
+				switch(st1)
+				{
+				case"Backwaren":
+					classOfGoods = ClassOfGoods.BAKERY_PRODUCTS;
+					break;
+				case"Milchprodukte":
+					classOfGoods = ClassOfGoods.DAIRY_PRODUCTS;
+					break;
+				case"Obst":
+					classOfGoods = ClassOfGoods.FRUITS;
+					break;
+				case"Gemüse":
+					classOfGoods = ClassOfGoods.VEGETABLES;
+					break;
+				default:
+					System.out.println("ClassOfGoods nicht vorhanden");
+					break;
+				}
+				String unit = tokens[3];
+				double price = Double.parseDouble(tokens[4]);
+				String st2 = tokens[5];
+				Currency currency = null;
+				switch(st2)
+				{
+				case"Euro":
+					currency = Currency.EURO;
+					break;
+				case"Dollar":
+					currency = Currency.DOLLAR;
+					break;
+				default:
+					System.out.println("Currency nicht vorhanden");
+					
+				}
+				Goods c = new Goods(id, des, classOfGoods, unit, price, currency);
+				goods.add(c);
+			}
+			scanner.close();
+		} catch (FileNotFoundException e) {
+		} catch (IOException e) {
+		}
+		
+	
+	}
+	
+	public void displayGoods()
+	{
+		System.out.println("Willkommen im " + name);
+		System.out.println();
+		System.out.println("Unser Sortiment: ");
+		System.out.println("ID  Bezeichnung   Warengruppe  Einheit  Preis  ");
+		for(Goods good : goods)
+		{
+			good.displayProduct();
+		}
+		System.out.println();
+	}
+
+}
