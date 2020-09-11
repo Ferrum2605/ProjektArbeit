e260e1eb30e7950cbb3201d329e5b9cb624947d5
A002520 <A002520@NB4535.all-for-one.net>
2020-09-11 09:45:10 +0200

    Jannik0944

--- Hausarbeit/src/pruefungsLeistung2/GameList.java
+++ Hausarbeit/src/pruefungsLeistung2/GameList.java
@@ -1,6 +1,44 @@
 package pruefungsLeistung2;
 
-public class GameList {
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class GameList implements Serializable{
+	
+private static final long serialVersionUID = 1L;
+	
+	
+	private ArrayList<Game> gameList;
+	
+	public GameList()
+	{
+		gameList = new ArrayList<>();
+	}
+	
+	public void addGame(Game game)
+	{
+		gameList.add(game);
+	}
+	
+	public void removeGame(Game game)
+	{
+		gameList.remove(game);
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public ArrayList<Game> getGameList() {
+		return gameList;
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 	
 
