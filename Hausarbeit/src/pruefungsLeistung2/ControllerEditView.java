7bcf0404d0fd0edd02c8111fea22568726343319
Lara Horvath <lara.horvath@cgm.com>
2020-09-11 09:26:42 +0200

    Lara

--- Hausarbeit/src/pruefungsLeistung2/ControllerEditView.java
+++ Hausarbeit/src/pruefungsLeistung2/ControllerEditView.java
@@ -1,5 +1,26 @@
 package pruefungsLeistung2;
 
-public class ControllerEditView {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+public class ControllerEditView implements Initializable {
+
+	
+	
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
